f627415ed8c7eeb20fd3fb776d4f8606982fd318
albertobiel <96720434+albertobiel@users.noreply.github.com>
2022-01-21 09:25:53 +0100

    Fix the problem with audit report

--- contract/Tetris.sol
+++ contract/Tetris.sol
@@ -1,12 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.2 <0.9.0;
+pragma solidity ^0.8.10;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
 contract Tetris is Ownable{
 
-    using SafeMath for uint;
+    //Account Structure for their own game
+    struct accountGame {
+        address connectedAccount1;
+        address connectedAccount2;
+    }
+
+    //Store the accounts as the game ID
+    mapping(uint => accountGame) public gameControl;
 
     //Store the amount following address to the betBNBamount
     mapping(address => uint256) public betBNBamount;
@@ -14,49 +20,48 @@ contract Tetris is Ownable{
     //Using isbetting mapping for the flag of the bet performance
     mapping(address => bool) public isbetting;
 
-    //Betting Account Array
-    address[] connectedAccount;
-
     //Contract comision
     uint16 feePercent = 10;
-    
+
+    //Event return token to the Owner
+    event ReturnToOwner(uint256 amount);
+
+    //Event return token to the Winner
+    event WithDraw(address winner, uint256 amount);
+
     //transaction successfully performed and receive BNB
     event Received (address , uint);
     
-    //Identify winner with connectedAccount
-    modifier onlyWinner(address _winner) {
-        require(connectedAccount[0] == _winner || connectedAccount[1] == _winner);
-        _;
+    //Return the GameState as the GameID
+    function getGameState(uint gameID) public view returns(address account1, address account2 ) {
+        accountGame memory accountGameById = gameControl[gameID];
+        return (accountGameById.connectedAccount1, accountGameById.connectedAccount2);
     }
-
-    //Return specific account's balance
-    function balanceOf(address _account) external view returns(uint256) {
-        return _account.balance;
+    
+    //Create the Game and Set GameID
+    function createGame(uint gameID, address account1, address account2) public returns(bool) {
+        accountGame memory newGame = accountGame(account1, account2);
+        gameControl[gameID] = newGame;
+        return true;
     }
-
     //Transform the BNB from account to the SC
     function transferBNB() payable public returns(bool){
         betBNBamount[msg.sender] = msg.value;
-        connectedAccount.push(msg.sender);
         emit Received(msg.sender, msg.value);
         return true;
     }
     
-    //Return the whole connectedAccount
-    function getAccounts() public view returns(address[] memory ) {
-        return connectedAccount;
-    }
-
     //Transform all of the SC's BNB.---for the test deploy.
     function returnToOwner() public{
         payable(owner()).transfer(address(this).balance);
+        emit ReturnToOwner(address(this).balance);
     }
 
     //Return the BNB to the winner, some comision to the owner.
-    function withdraw() external onlyWinner(msg.sender) payable {
-        uint256 amount = betBNBamount[connectedAccount[0]].add(betBNBamount[connectedAccount[1]]);
-        payable(msg.sender).transfer(amount.mul(100-feePercent).div(100));
-        payable(owner()).transfer(amount.mul(feePercent).div(100));
-        delete connectedAccount;
+    function withdraw(uint gameID) external payable {
+        uint256 amount = betBNBamount[gameControl[gameID].connectedAccount1]+(betBNBamount[gameControl[gameID].connectedAccount2]);
+        payable(msg.sender).transfer(amount*(100-feePercent)/(100));
+        payable(owner()).transfer(amount*(feePercent)/(100));
+        emit WithDraw(msg.sender, amount*(100-feePercent)/(100));
     }
 }
